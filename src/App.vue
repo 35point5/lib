9e10816fd635e46b7f19df0b473a26cc5c5c3e8f
Mo gician <464734297@qq.com>
2022-04-18 22:25:25 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <LibNavi msg="Welcome to Your Vue.js App"/>
+    <LibSearch class="ct"></LibSearch>
+  </div>
+</template>
+
+<script>
+import LibNavi from './components/LibNavi.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue'
+import LibSearch from "@/components/LibSearch";
+Vue.use(ElementUI)
+export default {
+  name: 'App',
+  components: {
+    LibSearch,
+    LibNavi
+  }
+}
+</script>
+
+<style>
+  #app{
+    /*position: absolute;*/
+    /*margin: auto;*/
+    /*top: 0;*/
+    /*left: 0;*/
+    /*bottom: 0;*/
+    /*right: 0;*/
+    height: 100%;
+  }
+</style>
---
+++ src/components/LibSearch.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container  class="search">
+  <el-main>
+    <el-input >
+
+    </el-input>
+  </el-main>
+  </el-container>
+</template>
+<script>
+
+</script>
+<style>
+  .search{
+    
+  }
+</style>
