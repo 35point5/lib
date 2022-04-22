dea41d7fc5a91429e47ac0dc8ba8a9561c244e3c
Mogician <Mogician@zju.edu.cn>
2022-04-22 14:01:36 +0800

    aaa

--- src/components/LibSearch.vue
+++ src/components/LibSearch.vue
@@ -3,7 +3,7 @@
   <el-main>
     <el-row type="flex" justify="center">
       <el-col :span="6">
-        <el-input>
+        <el-input v-model="query.Keywords">
 
         </el-input>
       </el-col>
@@ -13,40 +13,61 @@
             width="200"
             trigger="manual"
             v-model="visible">
-          <el-input v-for="(value,name) in bookInfo" :placeholder="name" :key="name" v-model="bookInfo[name]" style="margin-top: 10px"></el-input>
+          <el-input v-for="(value,name) in query.BookInfo" :placeholder="name" :key="name" v-model="query.BookInfo[name]" style="margin-top: 10px"></el-input>
           <el-button slot="reference" @click="cli()">高级选项</el-button>
         </el-popover>
       </el-col>
       <el-col :span="1">
-        <el-button @click="cli()">搜索</el-button>
+        <el-button @click="search()">搜索</el-button>
       </el-col>
+    </el-row>
+    <el-row v-for="item in this.result" :key="item.ISBN">
+      {{item.Title}}
     </el-row>
   </el-main>
   </el-container>
 </template>
 <script>
+// import axios from "axios";
+
 export default {
   data() {
     return {
       visible: false,
       text:"",
-      bookInfo:{
-        ISBN:"",
-        Title:"",
-        Author:"",
-        Publisher:"",
-        KeyWords:"",
-        Digest:"",
-        Category:"",
-        PublishTime:""
-      }
+      query:{
+        Keywords:"",
+        BookInfo:{
+          ISBN:"",
+          Title:"",
+          Author:"",
+          Publisher:"",
+          KeyWords:"",
+          Digest:"",
+          Category:"",
+          PublishTime:""
+        }
+      },
+      result:[],
+      activeNames:[]
     };
   },
   methods:{
     cli(){
       this.visible = !this.visible
       console.log(this.text)
-      console.log(this.bookInfo)
+      console.log(this.query)
+    },
+    search(){
+      const axios = require('axios');
+
+      axios.post('/lib/lookup',this.query).then((resp)=>{
+        this.result=resp.data.Data
+        console.log(this.result)
+      }).catch(function (error) {
+        console.log(error);
+      });
+
     }
   }
 };
